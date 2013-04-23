7393c945e541f857621221ea6619df100fd15b98
Kong XiangBo <yarshure@gmail.com>
2013-04-23 14:59:54 +0800

    first commit

--- Gear/KKViewController.h
+++ Gear/KKViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface KKViewController : UIViewController
+- (IBAction)startStopAction:(id)sender;
 
 @end
--- Gear/KKViewController.m
+++ Gear/KKViewController.m
@@ -9,6 +9,8 @@
 #import "KKViewController.h"
 
 @interface KKViewController ()
+@property (strong, nonatomic) IBOutlet UIImageView *gear;
+@property (strong, nonatomic) IBOutlet UIButton *actionBtn;
 
 @end
 
@@ -17,6 +19,13 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    NSMutableArray *images=[NSMutableArray arrayWithCapacity:36];
+    for (int i=0; i< 36; i++) {
+        [images addObject:[UIImage imageNamed:[NSString stringWithFormat: @"GearAnimation%d",i]]];
+    }
+    _gear.animationImages = images;
+    _gear.animationDuration = [_gear.animationImages count] / 30;
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -26,4 +35,13 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)startStopAction:(id)sender {
+    if ([_gear isAnimating]) {
+        [_gear stopAnimating];
+        [_actionBtn setTitle:@"Stop" forState:UIControlStateNormal];
+    }else{
+        [_gear startAnimating];
+        [_actionBtn setTitle:@"Start" forState:UIControlStateNormal];
+    }
+}
 @end
