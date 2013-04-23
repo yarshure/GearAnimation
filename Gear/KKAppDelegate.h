f990eb3f811326675f495e3a18edbbc65af208ff
Kong XiangBo <yarshure@gmail.com>
2013-04-23 14:45:33 +0800

    Initial Commit

---
+++ Gear/KKAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  KKAppDelegate.h
+//  Gear
+//
+//  Created by 孔祥波 on 13-4-23.
+//  Copyright (c) 2013年 Kong XiangBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class KKViewController;
+
+@interface KKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) KKViewController *viewController;
+
+@end
---
+++ Gear/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Gear
+//
+//  Created by 孔祥波 on 13-4-23.
+//  Copyright (c) 2013年 Kong XiangBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KKAppDelegate class]));
+    }
+}
